59692fa240f0687dad1f6e1afd45ce64597e8d44
LuisaoBR <luisfelipe.fs@outlook.com>
2024-09-10 18:40:56 -0300

    fix: metodo http

--- app/api/routes.py
+++ app/api/routes.py
@@ -3,7 +3,7 @@ from app.services.openai_service import validate_itinerary
 
 router = APIRouter()
 
-@router.get("/validar/")
+@router.post("/validar/")
 async def get_validated_itinerary(itinerary: dict):
     try:
         validated_itinerary = await validate_itinerary(itinerary)
